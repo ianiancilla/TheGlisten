86d13c27d8097767f9651d1edcddd81e956226f3
SLED\Iani <marzia.faustinelli@gmail.com>
2021-05-29 21:16:00 +0200

    flame velocity shader animation

---
+++ Assets/Scripts/Player and Health/FlameAnimation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FlameAnimation : MonoBehaviour
+{
+    [SerializeField] float velocityScaler = 1f;
+
+    // member variables
+    Vector3 previousPosition;
+
+    // cache
+    SpriteRenderer flameSpriteRenderer;
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        // cache
+        flameSpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        previousPosition = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimateFlame();
+    }
+
+    private void AnimateFlame()
+    {
+
+        if (transform.position != previousPosition)
+        {
+            Vector3 lastMovement = (transform.position - previousPosition).normalized;
+
+            flameSpriteRenderer.material.SetVector("_MovementSpeed", new Vector4(lastMovement.x * velocityScaler,                                                                                 lastMovement.y * velocityScaler));
+        }
+        else
+        {
+            flameSpriteRenderer.material.SetVector("_MovementSpeed", new Vector4(0,0));
+        }
+        previousPosition = transform.position;
+    }
+}
--- Assets/Scripts/Player and Health/Player_HealthGlisten.cs
+++ Assets/Scripts/Player and Health/Player_HealthGlisten.cs
@@ -8,7 +8,13 @@ public class Player_HealthGlisten : MonoBehaviour
     // properties
     [SerializeField] float maxHealth = 10f;
     [Tooltip ("The prefab of the light the player spawns when receiving damage")]
-    [SerializeField] GameObject HealthDropPrefab;
+
+    [Header ("Health drops")]
+    [SerializeField] GameObject healthDropPrefab;
+    [Tooltip ("In degrees, amount of variance in the direction health drops will be launched at on damage")]
+    [SerializeField] float healthDropDirectionNoise = 10f;
+    [SerializeField] float healthDropScatterForce = 1f;
+    [SerializeField] float healthDropVerticalForceScaler = 1.5f;
 
     // member variables
     private float lightIntensityMax;
@@ -48,29 +54,45 @@ public class Player_HealthGlisten : MonoBehaviour
         float intensityChange = lightIntensityMax * damageProportion;
         playerLight.intensity -= intensityChange;
 
-        // spawn HealthDrop and set its intensity
-        Vector3 dropPosition = CalculateDropPosition();
-        GameObject healthDrop = Instantiate(HealthDropPrefab, dropPosition, Quaternion.identity);
-        healthDrop.GetComponent<HealthDrop>().InitialiseDrop(damageProportion, damageTaken, intensityChange);
+        // spawn a HealthDrop for each point of damage taken, and make them fly off like Sonic rings
+        int numberOfDrops = (int)damageTaken;
+        float angleOfVelocity = 180f / (numberOfDrops + 1);
+
+        for (int i = 0; i < numberOfDrops; i++)
+        {
+            // create the drop
+            GameObject healthDrop = Instantiate(healthDropPrefab, transform.position, Quaternion.identity);
+
+            // shoot it off in appropriate direction, so that between them all drops form an arc
+            float velocityDegrees = angleOfVelocity * (i + 1 );
+            velocityDegrees += +Random.Range(0 - healthDropDirectionNoise, healthDropDirectionNoise);
+            velocityDegrees = Mathf.Clamp(velocityDegrees, 10, 170);
+
+            float velocityRadians = velocityDegrees * Mathf.Deg2Rad;
+                                    
+            Vector2 direction = new Vector2(Mathf.Cos(velocityRadians),
+                                            Mathf.Sin(velocityRadians)* healthDropVerticalForceScaler);
+            healthDrop.GetComponent<Rigidbody2D>().velocity = healthDropScatterForce * direction;
+            //Debug.Log("angle of velocity is " + angleOfVelocity + " velocity is " + velocityDegrees + "vector is " + direction);
+        }
+
+
     }
 
-    public void HealDamage (float damageHealed)
-    {
-        // increase health score
-        currentHealth += damageHealed;
+    // commented out as feature was removed
+    //public void HealDamage (float damageHealed)
+    //{
+    //    // increase health score
+    //    currentHealth += damageHealed;
 
-        // increase light intensity proportionally to damage taken
-        float healingProportion = damageHealed / maxHealth;
-        playerLight.intensity += lightIntensityMax * healingProportion;
-    }
+    //    // increase light intensity proportionally to damage taken
+    //    float healingProportion = damageHealed / maxHealth;
+    //    playerLight.intensity += lightIntensityMax * healingProportion;
+    //}
 
     private void Die()
     {
-        Debug.Log("Player died");
+        FindObjectOfType<GameOver>().TriggerGameOver();
     }
 
-    private Vector3 CalculateDropPosition()
-    {
-        return transform.position + new Vector3(0, -1, 0);
-    }
 }
