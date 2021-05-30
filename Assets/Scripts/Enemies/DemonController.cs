08f2d3e3ad302ec8043b25fc0ea7df5aa0671a46
Dave <davechezffm@gmail.com>
2021-05-30 18:36:11 +0200

    New Enemy Character createdl Still a bit buggy and needs improvement.

--- Assets/Scripts/Enemies/DemonController.cs
+++ Assets/Scripts/Enemies/DemonController.cs
@@ -1,44 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Animations;
 public class DemonController : MonoBehaviour
 {
     private Transform target;
-    public float range;
+    public float attackRange;
     private Animator myAnim;
+    private bool canMove;
+    public float moveRange;
+    public Transform homePosition;
+    public float moveSpeed;
     // Start is called before the first frame update
     void Start()
     {
         target = FindObjectOfType<PlayerController>().transform;
         myAnim = GetComponent<Animator>();
+        canMove = true;
      
 
     }
 
     // Update is called once per frame
     void Update()
+    {//The Enemy will chase the player when they are in a certain range.
+        if (Vector3.Distance(target.position, transform.position) <= moveRange)
+        {
+            if (canMove)
+            {
+                FollowPlayer();
+
+            }
+             }
+
+        //When the Player lleaves that range they will go back to a Home Position.
+        if (Vector3.Distance(target.position, transform.position) >= moveRange)
+        {
+            if (canMove)
+            {
+                GoHome();
+            }
+        }
+
+
+
+        //If the enemy gets real close to the player, they will do the flame attck.
+        if (Vector3.Distance(target.position, transform.position) < attackRange)
+
+          {
+              
+              if ((target.position.x - transform.position.x) < -0.1)
+              {
+                  myAnim.SetBool("AttackLeft", true);
+                  canMove = false;
+                Invoke("StopAttackLeft", 2f);
+               
+              }
+
+              if ((target.position.x - transform.position.x) > 0.01)
+              {
+
+                  myAnim.SetBool("AttackRight", true);
+                  canMove = false;
+                Invoke("StopAttckRight", 2f);
+              }          
+          }
+    }
+
+
+    void StopAttckRight()
+    {//This gets invoked after the attack animation has finished.
+        myAnim.SetBool("AttackRight", false);
+        myAnim.SetBool("Moving", true);
+        canMove = true;
+    }
+
+    void StopAttackLeft()
+    {//This gets invoked after the attack animation has finished.
+        myAnim.SetBool("AttackLeft", false);
+        myAnim.SetBool("Moving", true);
+        canMove = true;
+    
+    }
+    public void FollowPlayer()
     {
-        if (Vector3.Distance(target.position, transform.position) <= range)
-        {
-            if (target.position.x <= transform.position.x)
-            {
-                myAnim.SetBool("AttackLeft", true);
-            }
-
-            if (target.position.x >= transform.position.x)
-            {
-                myAnim.SetBool("AttackRight", true);
-            }
-
-
-        }
-
-        if (Vector3.Distance(target.position, transform.position) > range)
-        {
-            myAnim.SetBool("AttackRight", false);
-            myAnim.SetBool("AttackLeft", false);
-            myAnim.SetBool("Idle", true);
-        }
+        myAnim.SetFloat("MoveX", target.position.x - transform.position.x);
+        myAnim.SetFloat("MoveY", target.position.y - transform.position.y);
+        transform.position = Vector3.MoveTowards(transform.position,
+            target.transform.position, moveSpeed * Time.deltaTime);
+    }
+    public void GoHome()
+    {
+        myAnim.SetFloat("MoveX", homePosition.position.x - transform.position.x);
+        myAnim.SetFloat("MoveY", homePosition.position.y - transform.position.y);
+        transform.position = Vector3.MoveTowards(transform.position, homePosition.transform.position, moveSpeed * Time.deltaTime);
     }
 }
