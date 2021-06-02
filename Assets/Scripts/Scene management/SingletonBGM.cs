4eea1c787ff525cde3ad3434d3d64b3f7f022de4
SLED\Iani <marzia.faustinelli@gmail.com>
2021-06-02 23:53:32 +0200

    fire loop BGM

---
+++ Assets/Scripts/Scene management/SingletonBGM.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonBGM : MonoBehaviour
+{
+    // singleton enforcer
+    private void Awake()
+    {
+        if (FindObjectsOfType<SingletonBGM>().Length > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
