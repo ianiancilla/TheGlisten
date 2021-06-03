226e371bb681f0b85967c0e7c3e12a507ba7ca75
SLED\Iani <marzia.faustinelli@gmail.com>
2021-06-03 15:04:51 +0200

    rebased audio feature on master

---
+++ Assets/Scripts/ButtonSoundPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSoundPlayer : MonoBehaviour
+{
+    [SerializeField] AudioClip clickSound;
+
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = FindObjectOfType<SingletonBGM>().gameObject.GetComponent<AudioSource>();
+    }
+
+    public void ClickSound()
+    {
+        audioSource.PlayOneShot(clickSound);
+    }
+
+}
