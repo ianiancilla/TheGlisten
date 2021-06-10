3ed84236b13694bb263853576ae64cd9f6b479f5
Dave <davechezffm@gmail.com>
2021-06-10 10:53:22 +0200

    Updated Demon Animations. Added title screen music.

---
+++ Assets/Scripts/Scene management/titleScreenMusic.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class titleScreenMusic : MonoBehaviour
+{
+    private Scene scene;
+
+    private static titleScreenMusic instance = null;
+    public static titleScreenMusic Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+       
+        if (scene.name == "Cutscene")
+        {
+            Destroy(gameObject);
+        }
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Update()
+    {
+        scene = SceneManager.GetActiveScene();
+        if (scene.name == "Cutscene")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
